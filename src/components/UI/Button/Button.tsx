206a0e8822fad1e921cb929dcd567e57c77b7d92
Elmira Ilyasova <capricorn_26@mail.ru>
2022-09-04 02:11:29 +0700

    Correct Button component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -6,10 +6,10 @@ interface ButtonProps {
   type?: 'button' | 'reset' | 'submit',
   text: string,
   stretch?: boolean,
-  onClick: () => void,
+  onClick?: () => void,
 }
 
-const Button = ({type = 'button', text, stretch = false, onClick}: ButtonProps):JSX.Element => {
+const Button = ({type = 'button', text, stretch = false, onClick = null}: ButtonProps):JSX.Element => {
   return (
     <button className={`button
     ${stretch ? ' button--stretch' : ''}`}
